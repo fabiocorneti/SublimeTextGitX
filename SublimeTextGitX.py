067bc8a909d768e04fe9e9b67070f3c60fb2d38d
Fabio Corneti <info@corneti.com>
2012-01-04 13:19:23 +0100

    gitx_open command

---
+++ SublimeTextGitX.py
@@ -0,0 +1,36 @@
+import locale
+import os
+import subprocess
+import sublime, sublime_plugin
+
+class GitXCommand():
+    def get_path(self):
+        if self.window.active_view():
+            return self.window.active_view().file_name()
+        elif self.window.folders():
+            return self.window.folders()[0]
+        else:
+            sublime.status_message(__name__ + ': No place to open GitX to')
+            return False
+
+class GitxOpenCommand(sublime_plugin.WindowCommand, GitXCommand):
+    def is_enabled(self):
+        return True
+
+    def run(self, *args):
+        path = self.get_path()
+        if not path:
+            return
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+    
+        settings = sublime.load_settings('Base File.sublime-settings')
+        gitx_path = settings.get('gitx_path', None)
+        
+        if gitx_path in ['', None]:
+            sublime.error_message(__name__ + ': gitx executable path not set')
+            return False
+
+        sublime.status_message(gitx_path)
+        p = subprocess.Popen([gitx_path], cwd=path.encode(locale.getpreferredencoding(do_setlocale=True)), shell=True)
+
